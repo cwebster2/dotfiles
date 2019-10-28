19c981bbf104a9c6b3f7a3f389b3f2db48e5ebfa
Casey Webster <casey.webster@gmail.com>
2019-10-27 22:39:52 -0500

    Update install script; untested

---
+++ bin/install.sh
@@ -0,0 +1,418 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+# install.sh
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Choose a user account to use for this installation
+get_user() {
+	if [ -z "${TARGET_USER-}" ]; then
+		mapfile -t options < <(find /home/* -maxdepth 0 -printf "%f\\n" -type d)
+		# if there is only one option just use that user
+		if [ "${#options[@]}" -eq "1" ]; then
+			readonly TARGET_USER="${options[0]}"
+			echo "Using user account: ${TARGET_USER}"
+			return
+		fi
+
+		# iterate through the user options and print them
+		PS3='command -v user account should be used? '
+
+		select opt in "${options[@]}"; do
+			readonly TARGET_USER=$opt
+			break
+		done
+		fi
+	}
+
+check_is_sudo() {
+	if [ "$EUID" -ne 0 ]; then
+		echo "Please run as root."
+		exit
+	fi
+}
+
+
+setup_sources_min() {
+	apt update || true
+	apt install -y \
+		apt-transport-https \
+		ca-certificates \
+		curl \
+		dirmngr \
+		gnupg2 \
+		lsb-release \
+		--no-install-recommends
+
+	# turn off translations, speed up apt update
+	mkdir -p /etc/apt/apt.conf.d
+	echo 'Acquire::Languages "none";' > /etc/apt/apt.conf.d/99translations
+}
+
+# sets up apt sources
+setup_sources() {
+	setup_sources_min;
+
+	cat <<-EOF > /etc/apt/sources.list
+	deb http://httpredir.debian.org/debian sid main contrib non-free
+	deb-src http://httpredir.debian.org/debian/ sid main contrib non-free
+
+	deb http://httpredir.debian.org/debian experimental main contrib non-free
+	deb-src http://httpredir.debian.org/debian experimental main contrib non-free
+	EOF
+
+	# yubico
+	cat <<-EOF > /etc/apt/sources.list.d/yubico.list
+	deb http://ppa.launchpad.net/yubico/stable/ubuntu xenial main
+	deb-src http://ppa.launchpad.net/yubico/stable/ubuntu xenial main
+	EOF
+
+	# tlp: Advanced Linux Power Management
+	cat <<-EOF > /etc/apt/sources.list.d/tlp.list
+	# tlp: Advanced Linux Power Management
+	# http://linrunner.de/en/tlp/docs/tlp-linux-advanced-power-management.html
+	deb http://repo.linrunner.de/debian sid main
+	EOF
+
+	# Create an environment variable for the correct distribution
+	CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+	export CLOUD_SDK_REPO
+
+	# Add the Cloud SDK distribution URI as a package source
+	cat <<-EOF > /etc/apt/sources.list.d/google-cloud-sdk.list
+	deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main
+	EOF
+
+	# Import the Google Cloud Platform public key
+	curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+	# Add the Google Chrome distribution URI as a package source
+	cat <<-EOF > /etc/apt/sources.list.d/google-chrome.list
+	deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main
+	EOF
+
+	# Import the Google Chrome public key
+	curl https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+
+	# add the yubico ppa gpg key
+	apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 3653E21064B19D134466702E43D5C49532CBA1A9
+
+	# add the tlp apt-repo gpg key
+	apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 6B283E95745A6D903009F7CA641EED65CD4E8809
+}
+
+base_min() {
+	apt update || true
+	apt -y upgrade
+
+	apt install -y \
+		adduser \
+		automake \
+		bash-completion \
+    bat \
+		bc \
+		bzip2 \
+		ca-certificates \
+		coreutils \
+		curl \
+		dnsutils \
+    fd-find \
+		file \
+		findutils \
+		gcc \
+		git \
+		gnupg \
+		gnupg2 \
+		grep \
+		gzip \
+		hostname \
+		indent \
+		iptables \
+    iw \
+		jq \
+		less \
+		libc6-dev \
+		locales \
+		lsof \
+		make \
+		mount \
+		net-tools \
+		policykit-1 \
+		silversearcher-ag \
+		ssh \
+		strace \
+		sudo \
+		tar \
+		tree \
+		tzdata \
+		unzip \
+		vim \
+		xz-utils \
+		zip \
+		--no-install-recommends
+
+	apt autoremove
+	apt autoclean
+	apt clean
+
+	install_scripts
+}
+
+# installs base packages
+# the utter bare minimal shit
+base() {
+	base_min;
+
+	apt update || true
+	apt -y upgrade
+
+	apt install -y \
+		apparmor \
+		bridge-utils \
+		cgroupfs-mount \
+		fwupd \
+		fwupdate \
+		gnupg-agent \
+		google-cloud-sdk \
+		iwd \
+		libapparmor-dev \
+		libimobiledevice6 \
+		libltdl-dev \
+		libpam-systemd \
+		libseccomp-dev \
+    pcscd \
+		pinentry-curses \
+		scdaemon \
+		systemd \
+		--no-install-recommends
+
+	setup_sudo
+
+	apt autoremove
+	apt autoclean
+	apt clean
+}
+
+# setup sudo for a user
+# because fuck typing that shit all the time
+# just have a decent password
+# and lock your computer when you aren't using it
+# if they have your password they can sudo anyways
+# so its pointless
+# i know what the fuck im doing ;)
+setup_sudo() {
+	# add user to sudoers
+	adduser "$TARGET_USER" sudo
+
+	# add user to systemd groups
+	# then you wont need sudo to view logs and shit
+	gpasswd -a "$TARGET_USER" systemd-journal
+	gpasswd -a "$TARGET_USER" systemd-network
+
+	# create docker group
+	sudo groupadd docker
+	sudo gpasswd -a "$TARGET_USER" docker
+
+	# add go path to secure path
+	{ \
+		echo -e "Defaults	secure_path=\"/usr/local/go/bin:/home/${TARGET_USER}/.go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/share/bcc/tools:/home/${TARGET_USER}/.cargo/bin\""; \
+		echo -e 'Defaults	env_keep += "ftp_proxy http_proxy https_proxy no_proxy GOPATH EDITOR"'; \
+		echo -e "${TARGET_USER} ALL=(ALL) NOPASSWD:ALL"; \
+		echo -e "${TARGET_USER} ALL=NOPASSWD: /sbin/ifconfig, /sbin/ifup, /sbin/ifdown, /sbin/ifquery"; \
+	} >> /etc/sudoers
+
+	# setup downloads folder as tmpfs
+	# that way things are removed on reboot
+	# i like things clean but you may not want this
+	mkdir -p "/home/$TARGET_USER/Downloads"
+	echo -e "\\n# tmpfs for downloads\\ntmpfs\\t/home/${TARGET_USER}/Downloads\\ttmpfs\\tnodev,nosuid,size=2G\\t0\\t0" >> /etc/fstab
+}
+
+# install rust
+install_rust() {
+	curl https://sh.rustup.rs -sSf | sh
+}
+
+# install graphics drivers
+install_graphics() {
+	local system=$1
+
+	if [[ -z "$system" ]]; then
+		echo "You need to specify whether it's intel, geforce or optimus"
+		exit 1
+	fi
+
+	local pkgs=( xorg xserver-xorg xserver-xorg-input-libinput xserver-xorg-input-synaptics )
+
+	case $system in
+		"intel")
+			pkgs+=( xserver-xorg-video-intel )
+			;;
+		"geforce")
+			pkgs+=( nvidia-driver )
+			;;
+		"optimus")
+			pkgs+=( nvidia-kernel-dkms bumblebee-nvidia primus )
+			;;
+		*)
+			echo "You need to specify whether it's intel, geforce or optimus"
+			exit 1
+			;;
+	esac
+
+	apt update || true
+	apt -y upgrade
+
+	apt install -y "${pkgs[@]}" --no-install-recommends
+}
+
+# install stuff for i3 window manager
+install_wmapps() {
+	apt update || true
+	apt install -y \
+		alsa-utils \
+		feh \
+		i3 \
+		i3lock-fancy \
+		i3status \
+		flameshot \
+		suckless-tools \
+		kitty \
+		usbmuxd \
+		xclip \
+		compton \
+		--no-install-recommends
+
+}
+
+get_dotfiles() {
+	# create subshell
+	(
+	cd "$HOME"
+  #todo install ssh key from lastpass
+
+	if [[ ! -d "${HOME}/.dotfiles" ]]; then
+		# install dotfiles from repo
+    git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME clone git@github.com:cwebster2/dotfiles.git "${HOME}/.dotfiles"
+	fi
+
+	# enable dbus for the user session
+	# systemctl --user enable dbus.socket
+
+	cd "$HOME"
+	)
+
+	install_vim;
+}
+
+install_vim() {
+	# create subshell
+	(
+	cd "$HOME"
+
+	# install .vim files
+	sudo rm -rf "${HOME}/.vim"
+	git clone --recursive git@github.com:cwebster2/vim.git "${HOME}/.vim"
+	(
+	cd "${HOME}/.vim"
+	)
+
+	# update alternatives to vim
+	sudo update-alternatives --install /usr/bin/vi vi "$(command -v vim)" 60
+	sudo update-alternatives --config vi
+	sudo update-alternatives --install /usr/bin/editor editor "$(command -v vim)" 60
+	sudo update-alternatives --config editor
+	)
+}
+
+install_tools() {
+	echo "Installing golang..."
+	echo
+	install_golang;
+
+	echo
+	echo "Installing rust..."
+	echo
+	install_rust;
+
+	echo
+	echo "Installing scripts..."
+	echo
+	sudo install.sh scripts;
+}
+
+usage() {
+	echo -e "install.sh\\n\\tThis script installs my basic setup for a debian laptop\\n"
+	echo "Usage:"
+	echo "  base                                - setup sources & install base pkgs"
+	echo "  basemin                             - setup sources & install base min pkgs"
+	echo "  graphics {intel, geforce, optimus}  - install graphics drivers"
+	echo "  wm                                  - install window manager/desktop pkgs"
+	echo "  dotfiles                            - get dotfiles"
+	echo "  vim                                 - install vim specific dotfiles"
+	echo "  golang                              - install golang and packages"
+	echo "  rust                                - install rust"
+	echo "  scripts                             - install scripts"
+	echo "  tools                               - install golang, rust, and scripts"
+	echo "  dropbear                            - install and configure dropbear initramfs"
+}
+
+main() {
+	local cmd=$1
+
+	if [[ -z "$cmd" ]]; then
+		usage
+		exit 1
+	fi
+
+	if [[ $cmd == "base" ]]; then
+		check_is_sudo
+		get_user
+
+		# setup /etc/apt/sources.list
+		setup_sources
+
+		base
+	elif [[ $cmd == "basemin" ]]; then
+		check_is_sudo
+		get_user
+
+		# setup /etc/apt/sources.list
+		setup_sources_min
+
+		base_min
+	elif [[ $cmd == "graphics" ]]; then
+		check_is_sudo
+
+		install_graphics "$2"
+	elif [[ $cmd == "wm" ]]; then
+		check_is_sudo
+
+		install_wmapps
+	elif [[ $cmd == "dotfiles" ]]; then
+		get_user
+		get_dotfiles
+	elif [[ $cmd == "vim" ]]; then
+		install_vim
+	elif [[ $cmd == "rust" ]]; then
+		install_rust
+	elif [[ $cmd == "golang" ]]; then
+		install_golang "$2"
+	elif [[ $cmd == "scripts" ]]; then
+		install_scripts
+	elif [[ $cmd == "tools" ]]; then
+		install_tools
+	elif [[ $cmd == "dropbear" ]]; then
+		check_is_sudo
+
+		get_user
+
+		install_dropbear
+	else
+		usage
+	fi
+}
+
+main "$@"
