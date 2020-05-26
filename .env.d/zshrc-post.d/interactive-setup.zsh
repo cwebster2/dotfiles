c1c631d0f371be6690df4aac7f6fe57df2c9bc5e
Casey Webster <cwebster@glgroup.com>
2020-05-26 11:28:23 -0500

    Add glggroup bash completion

--- .env.d/zshrc-post.d/interactive-setup.zsh
+++ .env.d/zshrc-post.d/interactive-setup.zsh
@@ -2,6 +2,10 @@ kitty + complete setup zsh | source /dev/stdin
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
+if [ ! -z $(command -v glgroup) ]; then
+  source <(glgroup bashcomplete)
+fi
+
 if [ ! -z $(command -v navi) ]; then
   source <(navi widget zsh)
 fi
