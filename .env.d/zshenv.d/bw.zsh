cb862c78b0d776c1cca67dae3fc428188479d80c
Casey Webster <casey.webster@gmail.com>
2021-02-18 12:22:51 -0600

    Take out an echo

--- .env.d/zshenv.d/bw.zsh
+++ .env.d/zshenv.d/bw.zsh
@@ -4,7 +4,6 @@ function _bw_get_session() {
   if ! key_id=$(keyctl request user bw_session 2>/dev/null); then
     2>&1 bw login --check > /dev/null || bw login --apikey
     2>&1 bw unlock --check > /dev/null || BW_SESSION=$(bw unlock --raw)
-    echo $BW_SESSION
     key_id=$(echo "$BW_SESSION" | keyctl padd user bw_session @u)
   fi
   keyctl timeout "$key_id" $AUTO_LOCK
