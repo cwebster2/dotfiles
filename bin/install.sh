0195e5f96d35f54d9b65fa564dccf65309636ec6
Casey Webster <casey.webster@gmail.com>
2021-03-19 23:34:29 -0500

    -y

--- bin/install.sh
+++ bin/install.sh
@@ -185,7 +185,7 @@ install_emacs() {
     sudo rm -rf "${HOME}/.doom.d"
     git clone "https://github.com/hlissner/doom-emacs" "${HOME}/.emacs.d"
     git clone "https://github.com/cwebster2/.doom.d" "${HOME}/.doom.d"
-    "${HOME}/.emacs.d/bin/doom" install --yes
+    "${HOME}/.emacs.d/bin/doom" install -y
     cd "${HOME}/.doom.d"
     git remote set-url origin git@github.com:cwebster2/.doom.d
   )
