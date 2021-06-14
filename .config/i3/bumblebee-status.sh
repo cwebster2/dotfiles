985e91d528911d2a08013a4c674573f72674d21a
Casey Webster <casey.webster@gmail.com>
2021-06-14 16:34:45 -0500

    Take out docker_ps for now

--- .config/i3/bumblebee-status.sh
+++ .config/i3/bumblebee-status.sh
@@ -4,7 +4,7 @@ source ~/.fonts/fontawesome/*.sh
 source ~/miniconda3/bin/activate
 
 /home/casey/src/bumblebee-status/bumblebee-status \
-  -m spotify cpu cpu2 memory nic docker_ps shell pasink pasource datetime dunst \
+  -m spotify cpu cpu2 memory nic shell pasink pasource datetime dunst \
   -p datetime.format="%H:%M %a, %d %b" \
   nic.exclude="lo,docker*,veth,br-*,wg5" \
   nic.states="^down" \
@@ -13,7 +13,10 @@ source ~/miniconda3/bin/activate
   cpu2.temp_pattern="temp1_input" \
   shell.command='echo ⌨ $(upower -i /org/freedesktop/UPower/devices/keyboard_dev_CA_D5_E5_3F_FA_06 | grep percentage | cut -f2 -d: | tr -d " %")%' \
   shell.interval="60" \
-  -t iceberg-dark-powerline  
+  spotify.layout="spotify.song" \
+  spotify.concise_controls="true" \
+  -t iceberg-dark-powerline
   # --markup=pango
 #  -t wal-powerline
+# docker_ps
   #-t solarized-dark-awesome \
