a9f54d444337126c93db53820e0d32a9e780cd4a
Casey Webster <casey.webster@gmail.com>
2021-10-24 12:51:46 -0500

    Update and rename installer scriipt

--- bin/update-nvim.sh
+++ bin/update-nvim.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/env bash
 
-mv ~/bin/nvim.appimage ~/bin/nvim.appimage.old
-curl -sfLo ~/bin/nvim.appimage https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-chmod 755 ~/bin/nvim.appimage
+NVIM_GIT_DIR=$HOME/src/neovim
+
+if [ ! -d "${NVIM_GIT_DIR}" ]; then
+  echo "Cloning neovim"
+  mkdir -p $(dirname "${NVIM_GIT_DIR}")
+  pushd $(dirname "${NVIM_GIT_DIR}") 2>/dev/null
+  git clone git@github.com:neovim/neovim $(basename "${NVIM_GIT_DIR}")
+  popd 2>/dev/null
+else
+  echo "Pulling latest neovim"
+  pushd "${NVIM_GIT_DIR}" 2>/dev/null
+  git pull
+  popd 2>/dev/null
+fi
+
+pushd "${NVIM_GIT_DIR}" 2>/dev/null
+make \
+  CMAKE_INSTALL_PREFIX="$HOME"/.local/ \
+  CMAKE_BUILD_TYPE=RelWithDebInfo \
+  -j 8
+make install
+popd 2>/dev/null
+
+which nvim
 nvim --version | head -1
