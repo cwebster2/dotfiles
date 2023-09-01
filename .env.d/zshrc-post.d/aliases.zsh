e2a2537449f0eb021080f028b52103694434777a
Casey Webster <cwebster@glgroup.com>
2023-09-01 14:15:49 -0500

    Updates to dotfiles

--- .env.d/zshrc-post.d/aliases.zsh
+++ .env.d/zshrc-post.d/aliases.zsh
@@ -25,6 +25,7 @@ alias dockerrmrf="docker rmi $(docker images -f 'dangling=true' -q)"
 alias dockercontainerrmrf="docker ps -a | grep Exit | cut -d ' ' -f 1 | xargs sudo docker rm"
 
 alias gcm='git checkout $(gh repo view --json "defaultBranchRef" --jq ".[] | .name")'
+alias git.branches="git for-each-ref --color=always --sort=-committerdate refs/heads refs/remotes --format='%(authordate:short) %(color:red)%(objectname:short) %(color:yellow)%(refname:short)%(color:reset) (%(color:green)%(committerdate:relative)%(color:reset)) %(authorname)'"
 
 alias icat="kitty +kitten icat"
 
--- .zshenv
+++ .zshenv
@@ -2,3 +2,5 @@ for file in ~/.env.d/zshenv.d/*; do
   source "$file"
 done
 . "$HOME/.cargo/env"
+
+alias assume="source assume"
